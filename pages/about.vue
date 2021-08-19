2df46f621375a55c15b746c9e08d1e681438461d
justYu2001 <jack4781012@gmail.com>
2021-08-19 19:50:29 +0800

    nuxt link

---
+++ pages/about.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1>About</h1>
+        <nuxt-link to="/">Go to Home</nuxt-link>
+        <nuxt-link to="/about">Go to About</nuxt-link>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Home</h1>
+        <nuxt-link to="/">Go to home</nuxt-link>
+        <nuxt-link to="/about" no-prefetch>Go to About</nuxt-link>
+    </div>
+</template>
+
+<style>
+.nuxt-link-exact-active{
+    background-color: #ccc;
+}
+
+.nuxt-link-prefetched{
+    text-decoration: none;
+}
+</style>
