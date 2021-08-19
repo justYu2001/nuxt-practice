001c201c808ac94adc67aa2fd3c4c5396ac95a75
justYu2001 <jack4781012@gmail.com>
2021-08-19 16:49:09 +0800

    routing

---
+++ pages/users/_id/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>ID:{{ id }}</h1>
+</template>
+
+<script>
+import { useRoute } from '@nuxtjs/composition-api';
+
+export default {
+    setup() {
+        const route = useRoute().value;
+
+        return {
+            id: route.params.id,
+        }
+    }
+}
+</script>
