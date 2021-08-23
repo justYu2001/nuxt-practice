8dda3c25c81a746941e3555edb608761dac004dc
justYu2001 <jack4781012@gmail.com>
2021-08-23 21:47:28 +0800

    layouts

---
+++ layouts/error.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1 v-if="error.statusCode === 404">Not found</h1>
+        <nuxt-link to="/">Home</nuxt-link>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["error"],
+}
+</script>
